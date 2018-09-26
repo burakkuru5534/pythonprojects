a610d472c80661abe879c7656eed690ba6c7c538
Burak Kuru <43616032+burakkuru5534@users.noreply.github.com>
2018-09-26 20:09:51 +0300

    Add files via upload

---
+++ Arayüz-Menüler.py
@@ -0,0 +1,53 @@
+from tkinter import *
+
+
+def islem1():
+    print("islem1 yapıldı")
+
+def islem2():
+    print("islem2 yapıldı")
+
+
+def islem3():
+    print("islem3 yapıldı")
+
+
+def islem4():
+    print("islem4 yapıldı")
+
+
+def islem5():
+    print("islem5 yapıldı")
+
+
+def islem6():
+    print("islem6 yapıldı")
+
+
+
+
+pencere= Tk()
+
+
+menu = Menu(pencere)
+
+pencere.config(menu= menu)
+
+altMenu= Menu(menu)
+altMenu2=Menu(menu
+              )
+menu.add_cascade(label= "Menu-1", menu=altMenu)
+altMenu.add_command(label= "islem-1", command= islem1)
+altMenu.add_command(label= "islem-2", command= islem2)
+altMenu.add_command(label= "islem-3", command= islem3)
+altMenu.add_separator()
+altMenu.add_command(label= "Çıkış",command= pencere.destroy)
+
+menu.add_cascade(label= "Menu-2", menu=altMenu2)
+altMenu2.add_command(label= "islem-4", command= islem4)
+altMenu2.add_command(label= "islem-5", command= islem5)
+altMenu2.add_command(label= "islem-6", command= islem6)
+altMenu2.add_separator()
+altMenu2.add_command(label= "Çıkış",command= pencere.destroy)
+
+pencere.mainloop()
