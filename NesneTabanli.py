a1bb5e9de3b793464ec186d4f2fd60bd4b826f42
Burak Kuru <43616032+burakkuru5534@users.noreply.github.com>
2018-09-28 16:26:04 +0300

    Add files via upload
    basic class and object definations

---
+++ NesneTabanli.py
@@ -0,0 +1,13 @@
+# class tanımlaması ve class ın nesnesini tanımlamak #
+
+class Sinifim():
+    #classın değişken ve fonksiyonları
+    sinif_degiskeni="değişken"
+    def sinif_fonk():
+        pass
+
+Nesnem = Sinifim()
+
+print(Nesnem.sinif_degiskeni)
+
+# damn it
