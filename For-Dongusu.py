09c5f103a75f3f6800f4d4df186c91601311bb63
Burak Kuru <43616032+burakkuru5534@users.noreply.github.com>
2018-09-28 15:59:35 +0300

    Add files via upload

---
+++ For-Dongusu.py
@@ -0,0 +1,14 @@
+# For Döngüsü #
+
+Liste1 = ["Türk","Öğün","Çalış","Güven!"]
+Liste2 = ["Ne","Mutlu","Türk'üm","Diyene!"]
+# range(başla,bitir,kaçar kaçar) şeklinde liste oluşturan fonk.
+for i in range(0,4):
+    print (Liste1[i])
+
+x=0
+while(x<len(Liste2)):
+    print("\n",Liste2[x])
+    x=x+1
+
+# For Döngüsü #
