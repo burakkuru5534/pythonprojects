5bd75ec4ad360dba26a9eb6f2ffa7462d9768956
Burak Kuru <43616032+burakkuru5534@users.noreply.github.com>
2018-09-28 16:17:50 +0300

    Add files via upload

---
+++ MathModülü.py
@@ -0,0 +1,12 @@
+#math modulü #
+
+import math
+
+print(dir(math))
+
+print(math.ceil(10.543)) # Float sayıyı bir üste int olarak tamamlıyor.
+print(math.floor(11.99))
+
+     
+print(math.pi)
+print(math.log(math.pi,10))
